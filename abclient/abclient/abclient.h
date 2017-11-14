a4eb0231657e58231470dd5eac8f8c3f207fb7e5
sa <stievie2006@gmail.com>
2017-11-14 08:47:51 +0100

    Stuff

---
+++ abclient/abclient/abclient.h
@@ -0,0 +1,92 @@
+//
+// Copyright (c) 2008-2016 the Urho3D project.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#pragma once
+
+#include <Urho3D/Core/CoreEvents.h>
+#include <Urho3D/Engine/Application.h>
+#include <Urho3D/Engine/Engine.h>
+#include <Urho3D/Input/Input.h>
+#include <Urho3D/Input/InputEvents.h>
+#include <Urho3D/Resource/ResourceCache.h>
+#include <Urho3D/Resource/XMLFile.h>
+#include <Urho3D/IO/Log.h>
+#include <Urho3D/UI/UI.h>
+#include <Urho3D/UI/Text.h>
+#include <Urho3D/UI/Font.h>
+#include <Urho3D/UI/Button.h>
+#include <Urho3D/UI/UIEvents.h>
+#include <Urho3D/Scene/Scene.h>
+#include <Urho3D/Scene/SceneEvents.h>
+#include <Urho3D/Graphics/Graphics.h>
+#include <Urho3D/Graphics/Camera.h>
+#include <Urho3D/Graphics/Geometry.h>
+#include <Urho3D/Graphics/Renderer.h>
+#include <Urho3D/Graphics/DebugRenderer.h>
+#include <Urho3D/Graphics/Octree.h>
+#include <Urho3D/Graphics/Light.h>
+#include <Urho3D/Graphics/Model.h>
+#include <Urho3D/Graphics/StaticModel.h>
+#include <Urho3D/Graphics/Material.h>
+#include <Urho3D/Graphics/Skybox.h>
+
+using namespace Urho3D;
+
+/// This first example, maintaining tradition, prints a "Hello World" message.
+/// Furthermore it shows:
+///     - Using the Sample / Application classes, which initialize the Urho3D engine and run the main loop
+///     - Adding a Text element to the graphical user interface
+///     - Subscribing to and handling of update events
+class ClientApp : public Application
+{
+    URHO3D_OBJECT(ClientApp, Application);
+
+public:
+    /// Construct.
+    ClientApp(Context* context);
+
+    virtual void Setup();
+    
+    /// Setup after engine initialization and before running the main loop.
+    virtual void Start();
+    virtual void Stop();
+        
+protected:
+
+private:
+    int framecount_;
+    float time_;
+    SharedPtr<Text> text_;
+    SharedPtr<Scene> scene_;
+    SharedPtr<Node> boxNode_;
+    SharedPtr<Node> cameraNode_;
+    
+    /// Handle the logic update event.
+    void HandleUpdate(StringHash eventType, VariantMap& eventData);
+    void HandleBeginFrame(StringHash eventType, VariantMap& eventData);
+    void HandleKeyDown(StringHash eventType, VariantMap& eventData);
+    void HandleClosePressed(StringHash eventType, VariantMap& eventData);
+    void HandlePostUpdate(StringHash eventType, VariantMap& eventData);
+    void HandleRenderUpdate(StringHash eventType, VariantMap & eventData);
+    void HandlePostRenderUpdate(StringHash eventType, VariantMap & eventData);
+    void HandleEndFrame(StringHash eventType, VariantMap & eventData);
+};
