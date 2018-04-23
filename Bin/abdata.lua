88cfee5e1f953ba3f6bd96dc6dad60865b83ff12
sa <stievie2006@gmail.com>
2018-04-23 15:38:24 +0200

    Added

---
+++ Bin/abdata.lua
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------------------
+-- abdata Settings -------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+read_only = false
+
+data_port = 2770
+-- Max memory size, 1GB
+max_size = 1024 * 1024 * 1024
+
+-- log_dir = "c:/Users/Stefan Ascher/Documents/Visual Studio 2015/Projects/ABx/abserv/logs"
+
+require("config/db_test")
