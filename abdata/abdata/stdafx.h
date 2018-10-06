005d4ab037a7cb33ce187d16ed91c9acce8875a3
sa <sa@0x2a.wtf>
2018-10-06 09:09:20 +0200

    Fix

--- abdata/abdata/stdafx.h
+++ abdata/abdata/stdafx.h
@@ -17,10 +17,14 @@
 
 #define ASIO_STANDALONE
 
+#if defined(_MSC_VER)
 #pragma warning(push)
 #pragma warning(disable: 4592)
+#endif
 #include <asio.hpp>
+#if defined(_MSC_VER)
 #pragma warning(pop)
+#endif
 
 #include <uuid.h>
 
