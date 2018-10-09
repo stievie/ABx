207ab054e16f2efbe2d7aa2d8c7fb3f03e6c3c24
sa <sa@0x2a.wtf>
2018-10-09 10:56:50 +0200

    Added

---
+++ sql/pg_copy.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem Copies a backup to the local PostreSQL server
+
+rem DROP all existing tables
+FOR /f "tokens=2 delims=|" %%G IN ('c:\PROGRA~1\POSTGR~1\10\bin\psql.exe --host localhost --username postgres --command="\dt" forgottenwars') DO (
+   c:\PROGRA~1\POSTGR~1\10\bin\psql.exe --host localhost --username postgres --command="DROP table if exists %%G cascade" forgottenwars
+   echo table %%G dropped
+)
+
+rem Get latest backup directory
+for /f "delims=" %%a in ('dir /b /ad-h /od "i:\bak\pgsql\*"') do set "latestDir=%%~a"
+
+rem Import last backup
+c:\PROGRA~1\POSTGR~1\10\bin\psql.exe -U postgres -d forgottenwars -f i:\bak\pgsql\%latestDir%\forgottenwars.sql
