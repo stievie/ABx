850ed30d32d176510e0d323557f4af07b57db437
sa <sa@0x2a.wtf>
2018-10-11 16:09:01 +0200

    Stuff

---
+++ abscommon/abscommon/ThreadPool.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "ThreadPool.h"
+
+namespace Asynch {
+
+ThreadPool ThreadPool::Instance;
+
+ThreadPool::~ThreadPool()
+{
+    Stop();
+}
+
+void ThreadPool::Start()
+{
+    if (pool_)
+        return;
+
+    pool_ = std::make_unique<thread_pool>(numThreads_);
+}
+
+void ThreadPool::Stop()
+{
+    if (pool_)
+        pool_.reset();
+}
+
+}
