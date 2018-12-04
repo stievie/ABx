351098d2bcc861110adc8500fec7c3ab343668a6
sa <sa@0x2a.wtf>
2018-12-04 10:52:25 +0100

    Maps

---
+++ Bin/data/scripts/games/pergamon.lua
@@ -0,0 +1,38 @@
+function onStart()
+  local portal = self:AddNpc("/scripts/actors/logic/portal.lua")
+  if (portal ~= nil) then
+    portal:SetName("Rhodes Arena")
+    -- Map ID where this portal leads to
+    portal:SetVarString("destination", "a13b71f8-fe19-4bf5-bba7-c7642c796c0f")
+    -- Will call onTrigger() when it collides
+    portal:SetTrigger(true)
+    local x = 93.8996
+    local z = -35.5445
+    local y = 32.1627
+    portal:SetPosition(x, y, z)
+  end
+end
+
+function onStop()
+end
+
+function onAddObject(object)
+--  print("Object added: " .. object:GetName())
+end
+
+function onRemoveObject(object)
+--  print("Object added: " .. object:GetName())
+end
+
+function onPlayerJoin(player)
+--  print("Player joined: " .. player:GetName())
+end
+
+function onPlayerLeave(player)
+--  print("Player left: " .. player:GetName())
+end
+
+-- Game Update
+function onUpdate(timeElapsed)
+--  print(timeElapsed)
+end
