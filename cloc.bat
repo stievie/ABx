47d34fc3f38a8263d3555a612d93d55443f2ce43
sa <sa@0x2a.wtf>
2018-12-20 06:51:11 +0100

    Stuff

--- cloc.bat
+++ cloc.bat
@@ -1,4 +1,5 @@
 @echo off
 
-cloc-1.72.exe abclient/abclient abdata/abdata abfile/abfile ablb/ablb ablogin/ablogin abmsgs/abmsgs abscommon/abscommon abdb/abdb absadmin/absadmin abserv/abserv absmath/absmath libabclient/libabclient
+cloc-1.72.exe --exclude-list-file=.clocignore abclient/abclient abdata/abdata abfile/abfile ablb/ablb ablogin/ablogin abmsgs/abmsgs abscommon/abscommon abdb/abdb absadmin/absadmin abserv/abserv absmath/absmath libabclient/libabclient genavmesh/genavmesh import/import Tests/Tests Bin
+
 pause
