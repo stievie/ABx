1cd956388bcd83a556a3194ce6bd3a194c4de585
sa <sa@0x2a.wtf>
2019-01-12 09:26:46 +0100

    Keygen

--- Bin/00compress.bat
+++ Bin/00compress.bat
@@ -2,4 +2,4 @@
 
 if exist abserv.zip del abserv.zip
 
-"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip abserv.zip abdata.exe abmsgs.exe ablogin.exe abserv.exe absmngr.exe abfile.exe ablb.exe absadmin.exe server.crt server.csr server.key abserver.dh nssm.exe 00run.bat *.dll *.lua config\* data\* file_root\* admin\* logs\*\.gitkeep recordings\.gitkeep ..\README.md
+"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip abserv.zip abdata.exe abmsgs.exe ablogin.exe abserv.exe absmngr.exe abfile.exe ablb.exe absadmin.exe keygen.exe server.crt server.csr server.key abserver.dh nssm.exe 00run.bat *.dll *.lua config\* data\* file_root\* admin\* logs\*\.gitkeep recordings\.gitkeep ..\README.md
