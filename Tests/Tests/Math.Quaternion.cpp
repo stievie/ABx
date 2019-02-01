34b61976184e56a3963da320c197631f07e39b53
sa <sa@0x2a.wtf>
2019-02-01 06:40:20 +0100

    Tests

--- Tests/Tests/Math.Quaternion.cpp
+++ Tests/Tests/Math.Quaternion.cpp
@@ -6,7 +6,7 @@
 
 // https://github.com/gareth-cross/quat/blob/master/test/quaternion_test.cpp
 
-TEST_CASE("Quaternion")
+TEST_CASE("Quaternion Construct")
 {
     SECTION("Construct")
     {
@@ -51,13 +51,39 @@ TEST_CASE("Quaternion")
         REQUIRE(aa.x_ == 0.0f);
         REQUIRE(aa.z_ == 0.0f);
     }
+}
+
+TEST_CASE("Quaternion Operations")
+{
+    SECTION("Addition")
+    {
+        Math::Quaternion quat1(1.0f, 2.0f, 3.0f, 4.0f);
+        Math::Quaternion quat2(1.0f, -2.0f, 3.0f, -4.0f);
+        Math::Quaternion add = quat1 + quat2;
+
+        REQUIRE(add.w_ == 0.0f);
+        REQUIRE(add.x_ == 6.0f);
+        REQUIRE(add.y_ == 0.0f);
+        REQUIRE(add.z_ == 2.0f);
+    }
 
     SECTION("Multiply")
     {
-        Math::Quaternion quat = Math::Quaternion::FromAxisAngle(Math::Vector3::UnitX, float(M_PI));
-        Math::Vector3 v = Math::Matrix4::FromQuaternion(quat) * Math::Vector3::UnitY;
+        Math::Quaternion quat1(1 / std::sqrt(2.0f), 1.0f / std::sqrt(2.0f), 0.0f, 0.0f);
+        Math::Quaternion quat2(1 / std::sqrt(2.0f), 0.0f, 1.0f / std::sqrt(2.0f), 0.0f);
+        Math::Quaternion mul = quat1 * quat2;
 
-//        REQUIRE(v.Equals(Math::Vector3::UnitY));
+        REQUIRE(fabs(mul.w_ - 0.5f) < 0.0001f);
+        REQUIRE(fabs(mul.x_ - 0.5f) < 0.0001f);
+        REQUIRE(fabs(mul.y_ - 0.5f) < 0.0001f);
+        REQUIRE(fabs(mul.z_ - 0.5f) < 0.0001f);
+
+        mul = quat1 * quat1.Conjugate();
+
+        REQUIRE(fabs(mul.w_) < 0.0001f);
+        REQUIRE(fabs(mul.x_) < 0.0001f);
+        REQUIRE(fabs(mul.y_) < 0.0001f);
+        REQUIRE(fabs(mul.z_ - 1.0f) < 0.0001f);
     }
     SECTION("Conjugate")
     {
@@ -70,5 +96,39 @@ TEST_CASE("Quaternion")
         Math::Quaternion quat(1.0f, 1.0f, 1.0f, 1.0f);
         Math::Quaternion inv = quat.Inverse();
         REQUIRE(inv.Equals(Math::Quaternion(0.25f, -0.25f, -0.25f, -0.25f)));
+    }
+}
+
+TEST_CASE("Quaternion Methods")
+{
+    SECTION("Scaling")
+    {
+        Math::Quaternion quat(1.0f, 2.0f, 3.0f, 4.0f);
+        quat = quat * 2.0f;
+        REQUIRE(fabs(quat.w_ - 4.0f) < 0.0001f);
+        REQUIRE(fabs(quat.x_ - 6.0f) < 0.0001f);
+        REQUIRE(fabs(quat.y_ - 8.0f) < 0.0001f);
+        REQUIRE(fabs(quat.z_ - 2.0f) < 0.0001f);
+
+        quat = quat * 0.5f;
+        REQUIRE(fabs(quat.w_ - 3.0f) < 0.0001f);
+        REQUIRE(fabs(quat.x_ - 4.0f) < 0.0001f);
+        REQUIRE(fabs(quat.y_ - 1.0f) < 0.0001f);
+        REQUIRE(fabs(quat.z_ - 2.0f) < 0.0001f);
+
+        quat = quat / 2.0f;
+        REQUIRE(fabs(quat.w_ - 2.0f) < 0.0001f);
+        REQUIRE(fabs(quat.x_ - 0.5f) < 0.0001f);
+        REQUIRE(fabs(quat.y_ - 1.0f) < 0.0001f);
+        REQUIRE(fabs(quat.z_ - 1.5f) < 0.0001f);
+    }
+    SECTION("Scaling")
+    {
+        Math::Quaternion q = Math::Quaternion::FromAxisAngle(Math::Vector3::UnitY, float(M_PI) / 3.0f);
+        REQUIRE(fabs(q.w_ - std::sqrt(3.0f) / 2.0f) < 0.0001f);
+        REQUIRE(fabs(q.x_) < 0.0001f);
+        REQUIRE(fabs(q.y_ - 0.5f) < 0.0001f);
+        REQUIRE(fabs(q.z_) < 0.0001f);
+
     }
 }
