0be1d256a55bb79ba5a6068a341652941d920a43
sa <sa@0x2a.wtf>
2019-05-03 12:46:26 +0200

    added

---
+++ absall/rebuild.sh
@@ -0,0 +1,9 @@
+export cbp2make="/home/sa/Dokumente/projects/cbp2make-stl-rev147-all/bin/Release/cbp2make"
+
+$cbp2make -in abs3rd.workspace -unix --keep-outdir
+$cbp2make -in absall.workspace -unix --keep-outdir
+$cbp2make -in abtests.workspace -unix --keep-outdir
+
+make -f abs3rd.workspace.mak
+make -f absall.workspace.mak
+make -f abtests.workspace.mak
