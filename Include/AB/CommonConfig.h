81825ab47d41240e9e639b63210734c0bdd47010
sa <sa@0x2a.wtf>
2019-06-18 17:29:58 +0200

    Comment

--- Include/AB/CommonConfig.h
+++ Include/AB/CommonConfig.h
@@ -16,6 +16,7 @@
 // If defined disable nagle's algorithm, this make the game play smoother
 // acceptor_.set_option(asio::ip::tcp::no_delay(true));
 // But I think this makes some problems. Try again, watch out for sudden disconnects!
+// Okay, let's use it, didn't see any negative effects lately.
 #define TCP_OPTION_NODELAY
 
 #define PLAYER_MAX_SKILLS 8
