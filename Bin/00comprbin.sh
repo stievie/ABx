c5a2198f3421de53a34e3ba90438de764f483cd3
sa <sa@0x2a.wtf>
2019-06-20 09:09:20 +0200

    Minor

--- Bin/00comprbin.sh
+++ Bin/00comprbin.sh
@@ -3,4 +3,4 @@
 # Compresses only binaries, no data
 if [ -f "./Bin.zip" ]; then rm -f "./Bin.zip"; fi
 
-7z a -tzip Bin.zip abdata abmsgs ablogin abserv absmngr abfile ablb absadmin keygen
+7z a -tzip Bin.zip abdata abmsgs ablogin abserv abfile ablb absadmin keygen
