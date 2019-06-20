914b68f93d47c9b9be0b704166d09f12a2eeb8b9
sa <sa@0x2a.wtf>
2019-06-20 09:27:49 +0200

    Added

---
+++ sql/pg_import.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+printf "Enter password: "
+read passwd
+export PGPASSWORD=$passwd
+
+# Delete old data
+psql -U postgres forgottenwars -t -c "SELECT 'drop table \"' || tablename || '\" cascade;' FROM pg_tables WHERE schemaname = 'public'" | psql -U postgres forgottenwars
+# Import new data
+psql -U postgres -d forgottenwars -f ./forgottenwars.sql
