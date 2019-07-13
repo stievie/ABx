0d3a49dd1cc072b28dec80bf1a41ff7b658f2f6b
sa <sa@0x2a.wtf>
2019-07-13 19:02:40 +0200

    Stuffed stuff

--- cloc.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cloc-1.72.exe --exclude-list-file=.clocignore abclient/abclient abdata/abdata abdb/abdb abfile/abfile ablb/ablb ablogin/ablogin abmsgs/abmsgs abscommon/abscommon abdb/abdb absadmin/absadmin abserv/abserv absmath/absmath libabclient/libabclient genavmesh/genavmesh import/import Tests/Tests Include/AB Bin
-
-pause
--- cloc.sh
+++
@@ -1,1 +0,0 @@
-cloc --exclude-list-file=.clocignore abclient/abclient abdata/abdata abdb/abdb abfile/abfile ablb/ablb ablogin/ablogin abmsgs/abmsgs absadmin/absadmin abscommon/abscommon abserv/abserv absmath/absmath keygen/keygen libabclient/libabclient genavmesh/genavmesh import/import Tests/Tests Include/AB Bin
