d1cc5e4edf7d04cea8acefd5b09ee4d4cfbfcc18
sa <sa@0x2a.wtf>
2019-07-20 10:55:36 +0200

    Confirm update

--- Bin/00updatedata.sh
+++ Bin/00updatedata.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-rsync -av --delete /mnt/hdd02/abx/data ./
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	rsync -av --delete /mnt/hdd02/abx/data ./
+fi
 
