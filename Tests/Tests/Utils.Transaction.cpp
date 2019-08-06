de2ab4f4a21d8fb4bcacaee2d161ffc810bf2edf
sa <sa@0x2a.wtf>
2019-08-06 10:21:08 +0200

    Transaction

---
+++ Tests/Tests/Utils.Transaction.cpp
@@ -0,0 +1,61 @@
+#include "stdafx.h"
+#include <catch.hpp>
+
+#include "Transaction.h"
+
+TEST_CASE("Transaction")
+{
+    struct MyType
+    {
+        std::string mystring;
+        unsigned uint;
+        std::vector<std::string> strings;
+        std::vector<unsigned> uints;
+
+        bool operator == (const MyType& other)
+        {
+            if (mystring.compare(other.mystring) != 0)
+                return false;
+            if (uint != other.uint)
+                return false;
+            if (strings.size() != other.strings.size())
+                return false;
+            size_t i = 0;
+            for (const auto& s : strings)
+            {
+                if (s.compare(other.strings[i]) != 0)
+                    return false;
+                ++i;
+            }
+            if (uints.size() != other.uints.size())
+                return false;
+            size_t i2 = 0;
+            for (auto u : uints)
+            {
+                if (u != other.uints[i2])
+                    return false;
+                ++i2;
+            }
+            return true;
+        }
+    };
+
+    MyType t1;
+    t1.mystring = "string";
+    t1.uint = 5;
+    t1.strings.push_back("strings1");
+    t1.strings.push_back("strings2");
+    t1.uints.push_back(1);
+    t1.uints.push_back(2);
+    MyType t2 = t1;
+
+    {
+        Utils::Transaction transaction(t1);
+        t1.mystring = "nothing";
+        t1.uint = 0;
+        t1.strings.clear();
+        t1.uints.clear();
+    }
+    bool equal = t1 == t2;
+    REQUIRE(equal);
+}
