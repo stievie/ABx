7da1857c361ed40e1a588696ca3706a7117d9818
sa <sa@0x2a.wtf>
2019-12-17 11:23:28 +0100

    Add dbtool to zip file

--- Bin/zip.bat
+++ Bin/zip.bat
@@ -2,7 +2,7 @@
 
 if exist abserv.zip del abserv.zip
 
-"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip abserv.zip abdata.exe abmsgs.exe abmatch.exe ablogin.exe abserv.exe abfile.exe ablb.exe absadmin.exe keygen.exe server.crt server.csr server.key abserver.dh 00run.bat *.dll *.lua config\* data\* file_root\* admin\README.md admin\templates\* admin\root\*.ico admin\root\css\* admin\root\fonts\* admin\root\images\* admin\root\js\* admin\root\files\.gitkeep logs\*\.gitkeep recordings\.gitkeep ..\README.md
+"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip abserv.zip abdata.exe abmsgs.exe abmatch.exe ablogin.exe abserv.exe abfile.exe ablb.exe absadmin.exe keygen.exe dbtool.exe server.crt server.csr server.key abserver.dh 00run.bat *.dll *.lua config\* data\* file_root\* admin\README.md admin\templates\* admin\root\*.ico admin\root\css\* admin\root\fonts\* admin\root\images\* admin\root\js\* admin\root\files\.gitkeep logs\*\.gitkeep recordings\.gitkeep ..\README.md
 
 rem Not added to the ZIP archive:
 rem - admin\root\vendors
