638773dd027385d6c3ad491acfee29ede4093102
sa <sa@0x2a.wtf>
2020-01-01 12:40:20 +0100

    Added missing versions

--- sql/schema.000.sql
+++ sql/schema.000.sql
@@ -613,4 +613,13 @@ INSERT INTO public.game_skills VALUES ('e7062cfe-daf2-4b50-8f07-d1b20c032355', 8
 INSERT INTO public.game_skills VALUES ('fab4bcad-5fe1-4616-bf29-b9148740c869', 1379, 'Glowing Gaze', '13582547-50f6-11e8-a7ca-02100700d6f0', 512, 0, 'Spell. Deal 5..50 fire damage. If the target is burning, you get 5 energy plus 1 Energy for every 2 ranks of Energy Storage.', 'Spell. Deal 5..50 fire damage. If the target is burning, you get 5 energy plus 1 Energy for every 2 ranks of Energy Storage.', '/Textures/Skills/placeholder.png', '/scripts/skills/fire_magic/glowing_gaze.lua', 0, '85d0ef81-50f4-11e8-a7ca-02100700d6f0', '', '');
 INSERT INTO public.game_skills VALUES ('eecf3e66-d7fb-499c-872f-2824e34fdae1', 40, 'Ether Feast', 'f7477bbe-50f5-11e8-a7ca-02100700d6f0', 512, 0, 'Sudden Death', 'Spell. Target loses 3 energy, you are healed for 20..65 health by lost energy point.', '/Textures/Skills/placeholder.png', '/scripts/skills/inspiration_magic/ether_feast.lua', 0, '85d0939b-50f4-11e8-a7ca-02100700d6f0', '', '');
 
+INSERT INTO public.versions (uuid, name, value, internal) VALUES (public.random_guid(), 'game_attributes', 1, 0);
+INSERT INTO public.versions (uuid, name, value, internal) VALUES (public.random_guid(), 'game_effects', 1, 0);
+INSERT INTO public.versions (uuid, name, value, internal) VALUES (public.random_guid(), 'game_item_chances', 1, 0);
+INSERT INTO public.versions (uuid, name, value, internal) VALUES (public.random_guid(), 'game_items', 1, 0);
+INSERT INTO public.versions (uuid, name, value, internal) VALUES (public.random_guid(), 'game_maps', 1, 0);
+INSERT INTO public.versions (uuid, name, value, internal) VALUES (public.random_guid(), 'game_music', 1, 0);
+INSERT INTO public.versions (uuid, name, value, internal) VALUES (public.random_guid(), 'game_professions', 1, 0);
+INSERT INTO public.versions (uuid, name, value, internal) VALUES (public.random_guid(), 'game_skills', 1, 0);
+
 INSERT INTO public.versions (uuid, name, value, internal) VALUES (public.random_guid(), 'schema', 0, 1);
