ec9c6237274f181e8facf57194e7c38f90a55f05
sa <sa@0x2a.wtf>
2020-02-23 08:25:02 +0100

    Typo

--- sql/schema.006.sql
+++ sql/schema.006.sql
@@ -1,4 +1,4 @@
--- Timestamp when the item was deleted up. If noot deleted this is 0.
+-- Timestamp when the item was deleted up. If not deleted this is 0.
 ALTER TABLE public.concrete_items ADD COLUMN deleted bigint NOT NULL DEFAULT 0::bigint;
 
 CREATE INDEX concrete_items_deleted ON concrete_items USING btree (deleted);
