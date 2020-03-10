faa875a82042936153a68a051d1ef963a3751cac
sa <sa@0x2a.wtf>
2020-03-10 11:09:29 +0100

    Added TinyExpr wrapper and tests

---
+++ Include/TinyExpr.hpp
@@ -0,0 +1,104 @@
+/**
+ * Copyright 2020 Stefan Ascher
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <tinyexpr.h>
+#include <vector>
+
+class TinyExpr
+{
+private:
+    std::vector<te_variable> variables_;
+    std::vector<char*> names_;
+    te_expr* expr_{ nullptr };
+    void AddFunc(const std::string& name, const void* ptr, int type)
+    {
+        char* n = _strdup(name.c_str());
+        names_.push_back(n);
+
+        te_variable var = { n, ptr, type };
+        variables_.push_back(std::move(var));
+    }
+public:
+    ~TinyExpr()
+    {
+        if (expr_)
+            te_free(expr_);
+        for (auto& n : names_)
+            free(n);
+        names_.clear();
+    }
+    void AddVariable(const std::string& name, double* value)
+    {
+        char* n = _strdup(name.c_str());
+        names_.push_back(n);
+        te_variable var = { n, value };
+        variables_.push_back(std::move(var));
+    }
+    void AddFunction(const std::string& name, double(*callback)())
+    {
+        AddFunc(name, callback, TE_FUNCTION0);
+    }
+    void AddFunction(const std::string& name, double(*callback)(double))
+    {
+        AddFunc(name, callback, TE_FUNCTION1);
+    }
+    void AddFunction(const std::string& name, double(*callback)(double, double))
+    {
+        AddFunc(name, callback, TE_FUNCTION2);
+    }
+    void AddFunction(const std::string& name, double(*callback)(double, double, double))
+    {
+        AddFunc(name, callback, TE_FUNCTION3);
+    }
+    void AddFunction(const std::string& name, double(*callback)(double, double, double, double))
+    {
+        AddFunc(name, callback, TE_FUNCTION4);
+    }
+    void AddFunction(const std::string& name, double(*callback)(double, double, double, double, double))
+    {
+        AddFunc(name, callback, TE_FUNCTION5);
+    }
+    void AddFunction(const std::string& name, double(*callback)(double, double, double, double, double, double))
+    {
+        AddFunc(name, callback, TE_FUNCTION6);
+    }
+    void AddFunction(const std::string& name, double(*callback)(double, double, double, double, double, double, double))
+    {
+        AddFunc(name, callback, TE_FUNCTION7);
+    }
+    int Compile(const std::string& expr)
+    {
+        if (expr_)
+            te_free(expr_);
+
+        int err;
+        expr_ = te_compile(expr.c_str(), variables_.data(), static_cast<int>(variables_.size()), &err);
+        return err;
+    }
+    double Evaluate()
+    {
+        if (!expr_)
+            return 0.0;
+        return te_eval(expr_);
+    }
+};
---
+++ Tests/Tests/TinyExpr.cpp
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2020 Stefan Ascher
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "stdafx.h"
+#include <catch.hpp>
+
+#include <TinyExpr.hpp>
+
+TEST_CASE("TinyExpr simple")
+{
+    TinyExpr expr;
+
+    int err = expr.Compile("(1+1)*2");
+    REQUIRE(err == 0);
+    double res = expr.Evaluate();
+    REQUIRE(res == Approx(4.0));
+}
+
+TEST_CASE("TinyExpr variables")
+{
+    TinyExpr expr;
+    double x = 3;
+    double y = 4.0f;
+    expr.AddVariable("x", &x);
+    expr.AddVariable("y", &y);
+
+    int err = expr.Compile("sqrt(x^2+y^2)");
+    REQUIRE(err == 0);
+    double res = expr.Evaluate();
+    REQUIRE(res == Approx(5.0));
+}
+
+static double func1(double arg)
+{
+    return arg;
+}
+
+TEST_CASE("TinyExpr function1")
+{
+    TinyExpr expr;
+    expr.AddFunction("foo", func1);
+
+    int err = expr.Compile("foo(1)");
+    REQUIRE(err == 0);
+    double res = expr.Evaluate();
+    REQUIRE(res == Approx(1.0));
+}
+
+static double func2(double arg1, double arg2)
+{
+    return arg1 + arg2;
+}
+
+TEST_CASE("TinyExpr function2")
+{
+    TinyExpr expr;
+    expr.AddFunction("foo", func2);
+
+    int err = expr.Compile("foo(1, 3)");
+    REQUIRE(err == 0);
+    double res = expr.Evaluate();
+    REQUIRE(res == Approx(4.0));
+}
+
+TEST_CASE("TinyExpr lambda")
+{
+    TinyExpr expr;
+    // No captures!
+    expr.AddFunction("foo", [](double arg) -> double
+    {
+        return arg * 2;
+    });
+
+    int err = expr.Compile("foo(1)");
+    REQUIRE(err == 0);
+    double res = expr.Evaluate();
+    REQUIRE(res == Approx(2.0));
+}
+
+TEST_CASE("TinyExpr lambda variable")
+{
+    TinyExpr expr;
+    double x = 5;
+    expr.AddVariable("x", &x);
+    // No captures!
+    expr.AddFunction("foo", [](double arg) -> double
+    {
+        return arg * 2;
+    });
+
+    int err = expr.Compile("foo(x)");
+    REQUIRE(err == 0);
+    double res = expr.Evaluate();
+    REQUIRE(res == Approx(10.0));
+}
