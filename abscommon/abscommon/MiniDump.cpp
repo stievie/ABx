2f9780669072ba12376c113d5e96fc4a7e964f3e
sa <sa@0x2a.wtf>
2020-04-02 14:20:39 +0200

    Use log dir or tmp dir to write crash dumps and call stacks

--- abscommon/abscommon/MiniDump.cpp
+++ abscommon/abscommon/MiniDump.cpp
@@ -1,24 +1,3 @@
-/**
- * Copyright 2017-2020 Stefan Ascher
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
- * of the Software, and to permit persons to whom the Software is furnished to do
- * so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 //
 // Copyright (c) 2008-2018 the Urho3D project.
 //
@@ -42,6 +21,15 @@
 //
 
 #include "stdafx.h"
+#include "Logger.h"
+#include <filesystem>
+
+std::string GetDumpDir()
+{
+    if (!IO::Logger::logDir_.empty())
+        return IO::Logger::logDir_;
+    return std::filesystem::temp_directory_path().string();
+}
 
 #if defined(AB_WINDOWS)
 
@@ -125,20 +113,20 @@ int WriteMiniDump(const char* applicationName, EXCEPTION_POINTERS* exceptionPoin
     strftime(dateTime, 50, "%Y-%m-%d-%H-%M-%S", (const tm*)&p);
     std::string dateTimeStr = std::string(dateTime);
 
-    char curDir[MAX_PATH];
-    GetCurrentDirectoryA(MAX_PATH, curDir);
-    std::string miniDumpDir = std::string(curDir) + std::string("\\dumps");
-
-    std::string fileName = miniDumpDir + "\\" + dateTimeStr + "_" + std::string(applicationName);
+    std::string fileName = GetDumpDir() + "\\" + dateTimeStr + "_" + std::string(applicationName);
     std::string miniDumpName = fileName + ".dmp";
 
-    CreateDirectoryA(miniDumpDir.c_str(), nullptr);
     HANDLE file = CreateFileA(miniDumpName.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE | FILE_SHARE_READ,
         nullptr, CREATE_ALWAYS, 0, nullptr);
 
-    BOOL success = MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(),
-        file, MiniDumpWithDataSegs, &info, nullptr, nullptr);
-    CloseHandle(file);
+    BOOL success = file != INVALID_HANDLE_VALUE;
+
+    if (success)
+    {
+        success = MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(),
+            file, MiniDumpWithDataSegs, &info, nullptr, nullptr);
+        CloseHandle(file);
+    }
 
     if (success)
     {
