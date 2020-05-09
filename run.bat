af773336b8377f4f323b2c586606bd54f8b7c235
sa <sa@0x2a.wtf>
2020-05-09 07:11:28 +0200

    Wait only when the program is not running

--- run.bat
+++ run.bat
@@ -3,10 +3,8 @@
 cd Bin
 
 rem Start these only if they are not running
-tasklist /nh /fi "imagename eq abdata.exe" | find /i "abdata.exe" > nul || (start abdata.exe)
-timeout /t 1
-tasklist /nh /fi "imagename eq abmsgs.exe" | find /i "abmsgs.exe" > nul || (start abmsgs.exe)
-timeout /t 1
+tasklist /nh /fi "imagename eq abdata.exe" | find /i "abdata.exe" > nul || (start abdata.exe && timeout /t 1)
+tasklist /nh /fi "imagename eq abmsgs.exe" | find /i "abmsgs.exe" > nul || (start abmsgs.exe && timeout /t 1)
 tasklist /nh /fi "imagename eq abfile.exe" | find /i "abfile.exe" > nul || (start abfile.exe)
 tasklist /nh /fi "imagename eq ablogin.exe" | find /i "ablogin.exe" > nul || (start ablogin.exe)
 
