31507ff30459a3f6e496c734aadf1021922f20b4
sa <sa@0x2a.wtf>
2020-05-12 08:19:14 +0200

    Added Bits tests

--- Include/sa/Bits.h
+++ Include/sa/Bits.h
@@ -21,6 +21,8 @@
 
 #pragma once
 
+#include <string>
+
 namespace sa {
 namespace bits {
 
@@ -36,22 +38,65 @@ void un_set(T& bit_set, U bits)
     bit_set &= ~static_cast<T>(bits);
 }
 
+// Flip bits 0101 -> 1010
+template <typename T>
+void flip(T& bit_set)
+{
+    constexpr size_t c = count<T>();
+    for (size_t i = 1; i <= c; ++i)
+    {
+        if (is_set(bit_set, 1 << (i - 1)))
+            un_set(bit_set, 1 << (i - 1));
+        else
+            set(bit_set, 1 << (i - 1));
+    }
+}
+
+// Test if single bit is set in bit_set
 template <typename T, typename U>
 [[nodiscard]] bool is_set(T bit_set, U bit)
 {
     return (bit_set & static_cast<T>(bit)) == static_cast<T>(bit) && (bit != 0 || bit_set == static_cast<T>(bit));
 }
 
+// Test if any bit of bits is set in bit_set
 template <typename T, typename U>
 [[nodiscard]] bool is_any_set(T bit_set, U bits)
 {
     return (bit_set & static_cast<T>(bits)) != 0 && (bits != 0 || bit_set == static_cast<T>(bits));
 }
 
+// Test if no bit in bits is set in bit_set, i.e. !is_any_set()
+template <typename T, typename U>
+[[nodiscard]] bool is_none_set(T bit_set, U bits)
+{
+    return !is_any_set(bit_set, bits);
+}
+
+// Test if all are set
 template <typename T, typename U>
 [[nodiscard]] bool equals(T bit_set, U bits)
 {
     return bit_set == static_cast<T>(bits);
+}
+
+// Count of bits
+template <typename T>
+constexpr size_t count()
+{
+    return sizeof(T) * 8;
+}
+
+// Convert the bit set to a string, returns e.g. 00000000000000000000000000001010 when bit 2 and bit 4 are set
+template <typename T>
+std::string to_string(T value)
+{
+    constexpr size_t c = count<T>();
+    std::string result;
+    result.resize(c);
+    for (size_t i = 1; i <= c; ++i)
+        result[c - i] = is_set(value, 1 << (i - 1)) ? '1' : '0';
+    return result;
 }
 
 }
---
+++ Tests/Tests/sa.Bits.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include <catch.hpp>
+
+#include <sa/Bits.h>
+
+static const uint32_t BIT1 = 1;
+static const uint32_t BIT2 = 1 << 1;
+static const uint32_t BIT3 = 1 << 2;
+static const uint32_t BIT4 = 1 << 3;
+static const uint32_t BIT5 = 1 << 4;
+static const uint32_t BIT6 = 1 << 5;
+static const uint32_t BIT7 = 1 << 6;
+static const uint32_t BIT8 = 1 << 8;
+
+TEST_CASE("Bits is_set")
+{
+    uint32_t bit_set = BIT2 | BIT4;
+    REQUIRE(sa::bits::is_set(bit_set, BIT4));
+    REQUIRE(sa::bits::is_set(bit_set, BIT2));
+    REQUIRE(!sa::bits::is_set(bit_set, BIT5));
+}
+
+TEST_CASE("Bits is_set_any")
+{
+    uint32_t bit_set = BIT2 | BIT4;
+    REQUIRE(sa::bits::is_any_set(bit_set, BIT4 | BIT5));
+    REQUIRE(!sa::bits::is_any_set(bit_set, BIT1 | BIT5));
+}
+
+TEST_CASE("Bits flip")
+{
+    uint32_t bit_set = BIT2 | BIT4;
+    REQUIRE(sa::bits::to_string(bit_set) == "00000000000000000000000000001010");
+    sa::bits::flip(bit_set);
+    REQUIRE(sa::bits::to_string(bit_set) == "11111111111111111111111111110101");
+}
+
+TEST_CASE("Bits count")
+{
+    REQUIRE(sa::bits::count<uint32_t>() == 32);
+    REQUIRE(sa::bits::count<uint64_t>() == 64);
+}
+
+TEST_CASE("Bits to_string")
+{
+    uint32_t bit_set = BIT2 | BIT4 | BIT8;
+    REQUIRE(sa::bits::to_string(bit_set) == "00000000000000000000000100001010");
+}
