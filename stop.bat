90526c845520cf9d015f6b836cc7f3d63945a61f
sa <sa@0x2a.wtf>
2020-05-24 11:10:23 +0200

    Kill absadmin if running

--- stop.bat
+++ stop.bat
@@ -3,7 +3,7 @@
 taskkill /IM abserv.exe
 taskkill /IM abmatch.exe
 taskkill /IM ablogin.exe
-timeout /T 1 /nobreak >nul
+taskkill /IM absadmin.exe
 taskkill /IM abfile.exe
 timeout /T 1 /nobreak >nul
 taskkill /IM abmsgs.exe
