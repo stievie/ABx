a9a26be86c10d19b3b3459508de72942ae27a9e8
sa <sa@0x2a.wtf>
2020-07-04 11:50:29 +0200

    Add weapon

---
+++ sql/schema.024.sql
@@ -0,0 +1,10 @@
+-- Add a two handed staff
+
+INSERT INTO public.game_items VALUES ('5664d215-363f-4182-aa05-a27a39d55309', 510, 'Sun Staff', '/scripts/items/weapons/twohanded/staffs.lua', '/Objects/SunStuff.xml', '/Textures/Icons/Items/SunStaff.png', 38, 0, 0, 0, '00000000-0000-0000-0000-000000000000', '', 20);
+
+UPDATE public.versions SET value = value + 1 WHERE name = 'game_items';
+
+INSERT INTO public.game_item_chances VALUES ('b2968f5a-617b-47e6-8539-764d5c50233f', 'a13b71f8-fe19-4bf5-bba7-c7642c796c0f', '5664d215-363f-4182-aa05-a27a39d55309', 50);
+INSERT INTO public.game_item_chances VALUES ('678b3a77-d737-449c-9234-18ba7d4aada2', '3c081fd5-3966-433a-bc61-50a33084eac2', '5664d215-363f-4182-aa05-a27a39d55309', 30);
+
+UPDATE public.versions SET value = 24 WHERE name = 'schema';
