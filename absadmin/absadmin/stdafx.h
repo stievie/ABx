f119f3c64766d58c5f8c898fb6a6e5319b5c87b0
sa <sa@0x2a.wtf>
2020-07-31 11:23:35 +0200

    Fix warning

--- absadmin/absadmin/stdafx.h
+++ absadmin/absadmin/stdafx.h
@@ -49,6 +49,13 @@
 #if !defined(USE_STANDALONE_ASIO)
 #define USE_STANDALONE_ASIO
 #endif
+
+PRAGMA_WARNING_PUSH
+PRAGMA_WARNING_DISABLE_MSVC(4702 4127)
+#   include <lua.hpp>
+#   include <kaguya/kaguya.hpp>
+PRAGMA_WARNING_POP
+
 #include "Servers.h"
 
 #include <json.hpp>
