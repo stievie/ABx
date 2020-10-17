6c848ba52b1d9bdc0376ebf94ccc3751cb108ded
sa <sa@0x2a.wtf>
2020-10-17 20:26:17 +0200

    Compress pak files

--- abclient/Setup/compress.bat
+++ abclient/Setup/compress.bat
@@ -2,7 +2,7 @@
 
 if exist fwclient.7z del fwclient.7z
 
-..\Tools\PackageTool.exe ..\bin\AbData\ AbData.pak
-..\Tools\PackageTool.exe ..\bin\SoundData\ SoundData.pak
+..\Tools\PackageTool.exe ..\bin\AbData\ AbData.pak -c
+..\Tools\PackageTool.exe ..\bin\SoundData\ SoundData.pak -c
 
 "%PROGRAMFILES%\7-Zip\7z.exe" a -t7z -mx fwclient.7z ..\bin\fw.exe ..\bin\abupdate.exe ..\bin\d3dcompiler_47.dll ..\bin\libcrypto-1_1-x64.dll ..\bin\libssl-1_1-x64.dll *.pak config.xml ..\README.md ..\LICENSE.txt ..\..\CREDITS.md
